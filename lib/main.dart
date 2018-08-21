27fe19f7fbc140741337f6aa41b1fa3ca1844a07
juancarlosgallegos <juan.gallegos@bachoco.net>
2018-08-21 09:32:47 -0500

    Test VSCode git

--- lib/main.dart
+++ lib/main.dart
@@ -57,16 +57,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {    
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      /*home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          //child: Text('Hello World'),
-          child: RandomWords(),
-        ),
-      ),*/
+      title: 'Welcome to Flutter',      
       home: new RandomWords(),
     );
   }
